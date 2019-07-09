1473c93b00c29955d27a344c52c7eb0fb49d11bb
vladk96 <vladk961@gmail.com>
2019-07-09 16:15:24 +0300

    Update functions in api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -21,15 +21,27 @@ const fetchData = url =>
     })
     .catch(error => console.error('Error:', error));
 
+const postData = (url, data) =>
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(result => result)
+    .catch(error => console.error('Error:', error));
+
 const fetchCountry = countryId =>
   fetchData(API.link + API.paths.countries + countryId);
 
 const fetchCity = cityId => fetchData(API.link + API.paths.cities + cityId);
 
-const fetchState = stateId => fetchData(API.link + API.paths.states + stateId);
+export const fetchState = stateId =>
+  fetchData(API.link + API.paths.states + stateId);
 
 export const fetchUsers = async () => {
-  const users = await fetchData(API.link + API.routes.users);
+  const users = await fetchData(API.link + API.paths.users);
 
   const changedUser = users.map(async user => {
     const city = await fetchCity(user.city_id);
@@ -47,4 +59,16 @@ export const fetchUsers = async () => {
   });
 
   return Promise.all([...changedUser]);
+};
+
+export const fetchCountries = () => fetchData(API.link + API.paths.countries);
+
+export const fetchStates = () => fetchData(API.link + API.paths.states);
+
+export const fetchCities = () => fetchData(API.link + API.paths.cities);
+
+export const postUserData = data => {
+  const url = API.link + API.paths.users;
+
+  return postData(url, data);
 };
